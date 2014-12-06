a96bf3e234d47fbab88a6773e45c86c3ec899ed3
nealhamilton <neal.robert.hamilton@gmail.com>
2014-12-06 14:37:23 -0500

    Added Player.js and Object.js classes.

---
+++ Player.js
@@ -0,0 +1,59 @@
+/**
+ * Created by Hamilton on 12/6/14.
+ */
+
+
+/**
+ * MUST HAVE GLOBALLY:
+ * -- context
+ * -- spritesheet
+ * --
+ * */
+
+function Player (x, y, w, h) {
+
+    this.x = x;
+    this.y = y;
+    this.width = w;
+    this.height = h;
+    this.drawX = 0;
+    this.drawY = 0;
+    this.speed = 2;
+    this.moves = 0;
+    this.holding = new Object(0, 0, 0, 0, 0);
+
+    this.move = function (value) { // accesor for user to craete move
+        this.moves += value;
+    };
+
+    this.update = function () { // called from game loop
+        if (this.moves > 1) { // moving to the right
+            this.drawX += this.speed;
+            this.moves--;
+            this.holding.setPosition(this.drawX+=this.speed, this.height/2);
+        }
+        else { // moving to the left
+            this.drawX -= this.speed;
+            this.moves++;
+            this.holding.setPosition(this.drawY-=this.speed, this.height/2);
+        }
+    };
+
+    this.grab = function (object) {
+        if (object.isMovable) {
+            this.holding = object;
+            if ( (this.x + this.width) < object.x ) { // player is left of object
+                this.holding.setPosition(this.drawX + 10, this.height / 2);
+            }
+            else { // player is right of the object
+                this.holding.setPosition(this.drawX - 10, this.height / 2);
+            }
+        }
+    };
+
+    this.draw = function () {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.drawImage(spritesheet, this.x, this.y, this.width, this.height, this.drawX, this.drawY ,this.width, this.height);
+    };
+}
+
